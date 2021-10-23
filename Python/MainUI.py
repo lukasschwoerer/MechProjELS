9253f0a423f6779e0b579228189938c55dfeaba4
DESKTOP-6135NEP\lu <lu@roman1.de>
2021-10-24 00:06:52 +0200

    Added custom Buttons to GUI

--- Python/MainUI.py
+++ Python/MainUI.py
@@ -1,11 +1,31 @@
+## Libraries Import
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.properties import ListProperty
+
+
+## Declaration of global Variables
+Mode = 'Normal'
+Feed = 0.018
 
 class Startseite(Screen):
-    pass
+
+    def btn_normal(self):
+        print('Normal')
+
+    def btn_gewinde(self):
+        print('Gewinde')
+
+    def btn_prev(self):
+        print('Previous')
+
+    def btn_next(self):
+        print('Next')
+        
+    b_color = ListProperty()
 
 class MetrischeGewinde(Screen):
     pass
