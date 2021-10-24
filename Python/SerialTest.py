70fe33240517f6004b5f7e5bc59f7b8c01b03f42
DESKTOP-6135NEP\lu <lu@roman1.de>
2021-10-24 16:10:06 +0200

    Added Serial Testfile

---
+++ Python/SerialTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 5)
+
+# Read initial String from Launchpad
+input_str = ser.readline()
+print('Read input from Launchpad: ' + input_str('utf-8').strip())
+
+while 1:
+    output_str = input('String to send: ')
+    ser.write(output_str)
+    input_str = ser.readline()
+    print('Read input from Launchpad: ' + input_str('utf-8').strip())
